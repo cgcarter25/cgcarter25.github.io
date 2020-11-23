2501c4ecc0ccc218cce40f1d3fc82ed9cbb289ed
cgcarter25 <53186194+cgcarter25@users.noreply.github.com>
2020-11-23 16:45:54 -0500

    added contact form php

--- mail.php
+++ mail.php
@@ -1,74 +1,49 @@
 <?php
+
 if($_POST) {
-    $firstname = "";
-    $visitor_email = "";
+    $name = "";
+    $email = "";
     $email_title = "";
-    $concerned_department = "";
-    $visitor_message = "";
+    $phonenumber = "";
+    $message = "";
     $email_body = "<div>";
 
-    if(isset($_POST['visitor_name'])) {
-        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+    if(isset($_POST['name'])) {
+        $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
         $email_body .= "<div>
-                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+                           <label><b>Name:</b></label>&nbsp;<span>".$name."</span>
                         </div>";
     }
 
-    if(isset($_POST['visitor_email'])) {
-        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
-        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
         $email_body .= "<div>
-                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
+                           <label><b>Email:</b></label>&nbsp;<span>".$email."</span>
                         </div>";
     }
 
-    if(isset($_POST['email_title'])) {
-        $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
+    if(isset($_POST['message'])) {
+        $message = htmlspecialchars($_POST['message']);
         $email_body .= "<div>
-                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email_title."</span>
+                           <label><b>Message:</b></label>
+                           <div>".$message."</div>
                         </div>";
-    }
-
-    if(isset($_POST['concerned_department'])) {
-        $concerned_department = filter_var($_POST['concerned_department'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Concerned Department:</b></label>&nbsp;<span>".$concerned_department."</span>
-                        </div>";
-    }
-
-    if(isset($_POST['visitor_message'])) {
-        $visitor_message = htmlspecialchars($_POST['visitor_message']);
-        $email_body .= "<div>
-                           <label><b>Visitor Message:</b></label>
-                           <div>".$visitor_message."</div>
-                        </div>";
-    }
-
-    if($concerned_department == "billing") {
-        $recipient = "billing@domain.com";
-    }
-    else if($concerned_department == "marketing") {
-        $recipient = "marketing@domain.com";
-    }
-    else if($concerned_department == "technical support") {
-        $recipient = "tech.support@domain.com";
-    }
-    else {
-        $recipient = "contact@domain.com";
     }
 
     $email_body .= "</div>";
 
     $headers  = 'MIME-Version: 1.0' . "\r\n"
     .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $visitor_email . "\r\n";
+    .'From: ' . $email . "\r\n";
 
     if(mail($recipient, $email_title, $email_body, $headers)) {
-        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+        echo "<p>Thank you for contacting us, $name. You will get a reply within 24 hours.</p>";
     } else {
         echo '<p>We are sorry but the email did not go through.</p>';
     }
 
 } else {
     echo '<p>Something went wrong</p>';
-}?>
+}
+?>
