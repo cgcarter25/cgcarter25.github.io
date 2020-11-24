02cc40eedf0cc479b9301a61d1d670cd751cea2a
cgcarter25 <53186194+cgcarter25@users.noreply.github.com>
2020-11-23 19:32:47 -0500

    contact form

--- mail.php
+++ mail.php
@@ -31,14 +31,16 @@ if($_POST) {
     }
 
     $recipient = "cgcarter25@gmail.com";
-    
+
+    $email_subject = "New Form Submission"
+
     $email_body .= "</div>";
 
     $headers  = 'MIME-Version: 1.0' . "\r\n"
     .'Content-type: text/html; charset=utf-8' . "\r\n"
     .'From: ' . $email . "\r\n";
 
-    if(mail($recipient, $email_body, $headers)) {
+    if(mail($recipient, $email_subject, $email_body, $headers)) {
         echo "<p>Thank you for contacting us, $name. You will get a reply within 24 hours.</p>";
     } else {
         echo '<p>We are sorry but the email did not go through.</p>';
