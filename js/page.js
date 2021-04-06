cc8099c578fbcfcb832316b2a7b84328d90ffb3b
cgcarter25 <cgcarter25@gmail.com>
2021-04-06 10:36:51 -0700

    added take a look inside and see also in animal tools

--- js/page.js
+++ js/page.js
@@ -1,5 +1,6 @@
 let windowWidth,
     windowHeight,
+    seeAlsoBoxWidth,
     transitionTime = '0.7s';
 
 function sizeHomeImage() {
@@ -8,6 +9,21 @@ function sizeHomeImage() {
   document.querySelector('.image-header').style.width = `${window.innerWidth}px`
   document.querySelector('.image-caption').style.top = `${windowHeight/10}px`
   document.querySelector('.page-content').style.top = `${windowHeight}px`
+
+  if (windowWidth <= 480) {
+    seeAlsoBoxWidth = windowWidth-50
+  } else if (windowWidth <=990 && windowWidth > 480) {
+    seeAlsoBoxWidth = windowWidth/2.7
+  } else if (windowWidth >990){
+    seeAlsoBoxWidth = windowWidth/4.5
+  }
+
+  let seeAlsoBoxes = document.getElementsByClassName('see-also-box');
+  for (let i = 0; i < seeAlsoBoxes.length; i++) {
+    seeAlsoBoxes[i].style.width = `${seeAlsoBoxWidth}px`;
+    document.querySelector(`#${seeAlsoBoxes[i].id}-image`).style.width = `${seeAlsoBoxWidth}px`;
+    document.querySelector(`#${seeAlsoBoxes[i].id}-image`).style.height = `${seeAlsoBoxWidth}px`;
+  }
 }
 
 sizeHomeImage();
